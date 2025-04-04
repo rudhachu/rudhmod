38eb83fca56f9ff83070daa0e585ec70b3cbeace
rudhachu <120007671+rudhachu@users.noreply.github.com>
2025-04-05 05:17:19 +0530

    Update eval.js

--- plugins/eval.js
+++ plugins/eval.js
@@ -59,11 +59,11 @@ const { saveContact,
   getName } = require("../lib/database/StoreDb");
 const config = require ('../config')
 rudhra({pattern: '> ?(.*)', fromMe: true,dontAddCommandList: true, desc: 'Run js code (evel)', type: 'misc'}, async (message, match, client) => {return});
-rudhra({on: 'text', fromMe: true, dontAddCommandList: true,desc: 'Run js code (evel)', type: 'misc'}, async (message, match, client) => {
-if(message.message.startsWith(">")){
+rudhra({on: 'text', fromMe: true, dontAddCommandList: true,desc: 'Run js code (evel)', type: 'misc'}, async (message, match, m, client, msg) => {
+if(message.text.startsWith(">")){
 const m = message;
 try {
-let evaled = await eval(`${message.message.replace(">","")}`) 
+let evaled = await eval(`${message.text.replace(">","")}`) 
 if (typeof evaled !== 'string') evaled = require('util').inspect(evaled); 
 await message.reply(evaled) 
 } catch (err) {
@@ -71,13 +71,13 @@ await message.reply(util.format(err))
 }}
 })
 
-rudhra({on:'text', fromMe: true,dontAddCommandList: true}, async (message, match, client) => {
-if (message.message.startsWith("$")) {
+rudhra({on:'text', fromMe: true,dontAddCommandList: true}, async (message, match, m, client, msg) => {
+if (message.text.startsWith("$")) {
 var m = message
 var conn = message.client
 const util = require('util')
 const json = (x) => JSON.stringify(x,null,2)
-try { let return_val = await eval(`(async () => { ${message.message.replace("$","")} })()`)
+try { let return_val = await eval(`(async () => { ${message.text.replace("$","")} })()`)
 if (return_val && typeof return_val !== 'string') return_val = util.inspect(return_val)
 if (return_val) await message.send(return_val || "No return value")} catch (e) {
 if (e) await message.send(util.format(e))}
