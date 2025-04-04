4907c180faa8c3b69b39e96860a1c15fc7895896
rudhachu <120007671+rudhachu@users.noreply.github.com>
2025-04-04 21:17:53 +0530

    Update system.js

--- plugins/system.js
+++ plugins/system.js
@@ -137,37 +137,37 @@ rudhra({
 		fromMe: mode,
 		description: "Show All Commands",
 		dontAddCommandList: true,
-	},
+},
 	async (message, query) => {
 		if (query) {
 			for (const plugin of commands) {
 				if (plugin.pattern && plugin.pattern.test(message.prefix + query)) {
-					const commandName = plugin.pattern.toString().split(/\W+/)[2]; // Changed this line
+					const commandName = plugin.pattern.toString().split(/\W+/)[2];
 					return message.reply(`\`\`\`Command: ${message.prefix}${commandName.trim()}
-Description: ${plugin.description || "No description available"}\`\`\``);
+                  Description: ${plugin.description || "No description available"}\`\`\``);
 				}
 			}
 			return message.reply("Command not found.");
 		} else {
 			const { prefix } = message;
 			const [currentDate, currentTime] = new Date().toLocaleString("en-IN", { timeZone: TIME_ZONE }).split(",");
-			let menuText = `\`\`\`╭───────────────
+			let menuText = `╭───────────────
 │ User: ${message.pushName}
 │ Prefix: ${prefix}
 │ Date: ${currentDate}
 │ Time: ${currentTime}
 │ Plugins: ${commands.length}
 │ Runtime: ${runtime(process.uptime())}
-╰────────────────\`\`\`\n`;
+╰────────────────\n`;
 
 			const commandList = [];
 			const categories = new Set();
 
 			commands.forEach(command => {
 				if (command.pattern && !command.dontAddCommandList) {
-					const commandName = command.pattern.toString().split(/\W+/)[2]; // Changed this line
+					const commandName = command.pattern.toString().split(/\W+/)[2];
 					const category = command.type ? command.type.toLowerCase() : "misc";
-					commandList.push({ name: commandName, category });
+					commandList.push({name: commandName, category});
 					categories.add(category);
 				}
 			});
@@ -176,12 +176,12 @@ Description: ${plugin.description || "No description available"}\`\`\``);
 			Array.from(categories)
 				.sort()
 				.forEach(category => {
-					menuText += `\n\`\`\`╭── ${category.toUpperCase()} ────`;
+					menuText += `\n╭── ${category.toUpperCase()} ────`;
 					const categoryCommands = commandList.filter(cmd => cmd.category === category);
-					categoryCommands.forEach(({ name }) => {
-						menuText += `\n│ ${name.toUpperCase().trim()}`;
+					categoryCommands.forEach(({name}) => {
+						menuText += `\n│ ${name}`;
 					});
-					menuText += `\n╰──────────────\`\`\`\n`;
+					menuText += `\n╰──────────────\n`;
 				});
 			return await message.send(menuText.trim());
 		}
